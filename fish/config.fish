e819846f825e55960a2e2c4449eaf8082c7bd5bf
Luka Sijic <23730024+luka-sijic@users.noreply.github.com>
2025-11-22 15:29:16 -0700

    update gitignore

---
+++ fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    if test -d /opt/homebrew/bin
+        fish_add_path /opt/homebrew/bin
+    end
+    # Commands to run in interactive sessions can go here
+    abbr --add cx 'gcc -std=c11 -Wall *.c -o temp_bin && ./temp_bin && rm temp_bin'
+    abbr --add cxx 'g++ -std=c++17 *.cpp -o temp_bin && ./temp_bin && rm temp_bin'
+    abbr --add g++ 'g++ -std=c++17'
+    abbr --add py python3
+    starship init fish | source
+end
